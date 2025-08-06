01239136abd83ed04fa955af6ddd3b640769809f
zaveriya-khan <khanzaveriya898@gmail.com>
2025-08-06 17:27:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function selectRamen(ramenName) {
+    alert(`Thank you for selecting ${ramenName}! Your delicious ramen will be ready shortly.`);
+  }
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+
+
+
