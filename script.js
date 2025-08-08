4862736d0d20568eb55ca3b7003566a2ccecdbe6
zaveriya-khan <khanzaveriya898@gmail.com>
2025-08-08 22:50:24 +0530

    Added Scroll-to-top Btn

--- script.js
+++ script.js
@@ -12,6 +12,17 @@ const swiper = new Swiper('.swiper', {
   },
 });
 
-
-
-
+const scrollToTopBtn = document.getElementById("scroll-to-top-btn");
+  window.onscroll = function () {
+  if(
+    document.body.scrollTop > 200 ||
+    document.documentElement.scrollTop > 200
+    ) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+      };
+      scrollToTopBtn.addEventListener("click", () => {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      });
